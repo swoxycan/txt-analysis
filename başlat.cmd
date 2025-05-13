d35afc64136ed6098c8ca34cc72555c9681067dd
Swoxy <211426428+swoxycan@users.noreply.github.com>
2025-05-13 20:33:46 +0300

    Add files via upload

---
+++ başlat.cmd
@@ -0,0 +1,12 @@
+@echo off
+color d
+title swoxy txt-analysis Kurulum basladi
+pip install colorama
+pip install style
+cls
+title kurulum basarili
+echo Kurulum basarili.
+cls
+title swoxy txt-analysis
+py main.py
+pause
